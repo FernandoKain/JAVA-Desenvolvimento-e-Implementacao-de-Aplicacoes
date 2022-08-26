87c401eac6d60435a7246dd95b40c1f2a66d7088
FernandoKain <fernandomanga@hotmail.com>
2022-08-26 19:34:11 -0300

    Exercício de médias com JOptionPane acrescentado

---
+++ src/cap02_exercicios/Exercicio04.java
@@ -0,0 +1,54 @@
+package cap02_exercicios;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+		/* Usando a classe JOptionPane para entrada de dados, elabore uma classe que receba a nota 
+		 * de duas provas e de um trabalho. Calcule e mostre a média e o resultado final (aprovado 
+		 * ou reprovado). Para calcular a média utilize a fórmula sequinte: 
+		 * 
+		 * média = (nota da prova 1 + nota da prova 2 + nota do trabalho) / 3
+		 * 
+		 * Considere que amédia mínima para aprovação é 6.
+		 * 
+		 */
+		
+		String aux;
+		float prova1;
+		float prova2;
+		float trabalho;
+		float media;
+		String resultado;
+		
+		try {
+			
+			aux = JOptionPane.showInputDialog("Entre com o valor da prova 1: ");
+			prova1 = Float.parseFloat(aux);
+			
+			aux = JOptionPane.showInputDialog("Entre com o valor da prova 2: ");
+			prova2 = Float.parseFloat(aux);
+			
+			aux = JOptionPane.showInputDialog("Entre com o valor do trabalho: ");
+			trabalho = Float.parseFloat(aux);
+			
+			media = (prova1 + prova2 + trabalho)/3;
+			
+			if(media >= 6) {
+				resultado = "APROVADO!";
+			}else {
+				resultado = "REPROVADO!";
+			}
+			
+			JOptionPane.showMessageDialog(null, "Sua média é: "+ media + " e você foi " + resultado);
+			
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+			JOptionPane.showMessageDialog(null, "Houve um erro de digitação." + e.toString());
+		}
+
+	}
+
+}
