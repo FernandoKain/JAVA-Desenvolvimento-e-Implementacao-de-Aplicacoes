93776257ae85d4b12467149aea9773e03cab97d2
FernandoKain <fernandomanga@hotmail.com>
2022-08-27 13:31:27 -0300

    Estrutura condicional com JOptionPane.PLAIN_MESSAGE (Caixa de seleção)

---
+++ src/cap03/If.java
@@ -0,0 +1,26 @@
+package cap03;
+
+import javax.swing.JOptionPane;
+
+public class If {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Object[] op = {"Masculino", "Feminino"};
+		String resp = (String)JOptionPane.showInputDialog(null, "Selecione o sexo: \n", "Pesquisa", 
+				JOptionPane.PLAIN_MESSAGE, null, op, "Masculino");
+		
+		if(resp == null) {
+			JOptionPane.showMessageDialog(null, "Você pressionou 'Cancel'.");
+		}
+		if (resp.equals("Masculino")) {
+			JOptionPane.showMessageDialog(null, "Você é homem.");
+		}
+		if(resp.equals("Feminino")) {
+			JOptionPane.showMessageDialog(null, "Você é mulher.");
+		}
+		System.exit(0);
+	}
+
+}
