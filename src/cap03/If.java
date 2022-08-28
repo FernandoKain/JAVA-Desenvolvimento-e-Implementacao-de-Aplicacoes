f1159018c7cc153f380ee46a403c9ee1aecef88c
FernandoKain <fernandomanga@hotmail.com>
2022-08-27 23:51:46 -0300

    Caixa de texto adequada para QUESTION_MESSAGE

--- src/cap03/If.java
+++ src/cap03/If.java
@@ -9,7 +9,7 @@ public class If {
 		
 		Object[] op = {"Masculino", "Feminino"};
 		String resp = (String)JOptionPane.showInputDialog(null, "Selecione o sexo: \n", "Pesquisa", 
-				JOptionPane.PLAIN_MESSAGE, null, op, "Masculino");
+				JOptionPane.QUESTION_MESSAGE, null, op, "Masculino");
 		
 		if(resp == null) {
 			JOptionPane.showMessageDialog(null, "Você pressionou 'Cancel'.");
