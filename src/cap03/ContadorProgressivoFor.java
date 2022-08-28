729a3c4ea72e2b6c682d557d91b4b4295b5d15d3
FernandoKain <fernandomanga@hotmail.com>
2022-08-28 11:15:40 -0300

    Laço for simples.

---
+++ src/cap03/ContadorProgressivoFor.java
@@ -0,0 +1,16 @@
+package cap03;
+
+public class ContadorProgressivoFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println("\nAcabou a contagem!");
+
+	}
+
+}
