c54c126ccacfc7e8e28404575f2eda073044c367
FernandoKain <fernandomanga@hotmail.com>
2022-08-28 12:01:14 -0300

    ContadorWhile adicionado. Contagem regressiva.

---
+++ src/cap03/ContadorWhile.java
@@ -0,0 +1,27 @@
+package cap03;
+
+import javax.swing.JOptionPane;
+
+public class ContadorWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			
+			int limite = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade: "));
+			int contador = limite;
+			
+			while(contador >= 0) {
+				System.out.println(contador);
+				contador--;
+			}
+			System.out.println("Fim do contador regressivo.");
+		}catch (NumberFormatException e) {
+			System.out.println("Não foi fornecido um número inteiro válido!\n" + e.toString());
+		}
+		System.exit(0);
+
+	}
+
+}
