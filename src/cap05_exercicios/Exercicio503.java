ffb9378629032ef2a62e13a88865727c55bee477
FernandoKain <fernandomanga@hotmail.com>
2022-10-30 16:20:01 -0300

    Populando um array bidimensional.

---
+++ src/cap05_exercicios/Exercicio503.java
@@ -0,0 +1,28 @@
+package cap05_exercicios;
+
+import java.util.Random;
+
+public class Exercicio503 {
+
+	public static void main(String[] args) {
+		/*Uma imagem é formada por pixels. Considere uma imagem com 
+		 * dimensão de 40 x 40 de faça uma classe que contenha um array bidimensional
+		 * com essas dimensões. A seguir, para cada posição desse array bidimensional 
+		 * armazene um valor aleatório entre 0 e 255 (esses valores correspondem às 
+		 * tonalidades apliadas sobre a imagem). Apresente em tela os 1600 valores 
+		 * gerados.
+		 */
+		
+		int[][] arrayDeCores = new int[40][40];
+		Random random = new Random();
+		
+		for (int linha = 0; linha < arrayDeCores.length; linha++) {
+			for (int coluna = 0; coluna < arrayDeCores[linha].length; coluna++) {
+				arrayDeCores[linha][coluna] = random.nextInt(255);
+				System.out.println("[" + linha + "," + coluna + "]" + arrayDeCores[linha][coluna]);
+			}
+		}
+
+	}
+
+}
